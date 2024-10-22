b32b02775d3fa2e4d7590574d74bf2fbd94acb99
FedericoCampi <federicocampi95@gmail.com>
2024-10-22 19:53:55 -0300

    variable de .env con NEXT_PUBL al comienzo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
--- src/components/FuturisticSpeedTest.tsx
+++ src/components/FuturisticSpeedTest.tsx
@@ -122,8 +122,8 @@ export default function FuturisticSpeedTest() {
     await new Promise(resolve => setTimeout(resolve, 3000))
 
 
-    console.log(process.env.FAST_TEST_URL)
-    const response = await axios.get(process.env.FAST_TEST_URL ?? 'localhost:3000/speedtest');
+    console.log(process.env.NEXT_PUBLIC_FAST_TEST_URL)
+    const response = await axios.get(process.env.NEXT_PUBLIC_FAST_TEST_URL || 'http://localhost:3000/speedtest');
 
     const { downloadSpeed, uploadSpeed, latency } = response.data;
 
